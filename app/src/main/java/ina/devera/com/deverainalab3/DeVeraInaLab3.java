d7b6cb1cce4d2b75affb5e75d71dc4ce8daac68d
whalien7oh7 <2015081484@ust-ics.mygbiz.com>
2018-09-08 14:49:28 +0800

    first commit

---
+++ app/src/main/java/ina/devera/com/deverainalab3/DeVeraInaLab3.java
@@ -0,0 +1,13 @@
+package ina.devera.com.deverainalab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeVeraInaLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_de_vera_ina_lab3);
+    }
+}
